b735a4ec8240071c33243785179d2e29b40ac56d
Daniel Matthews <d.matthews@ucl.ac.uk>
2026-04-21 18:55:38 +0100

    Update rbac

--- internal/controller/vaultk8sconfig_controller.go
+++ internal/controller/vaultk8sconfig_controller.go
@@ -116,6 +116,8 @@ type VaultK8sConfigReconciler struct {
 // +kubebuilder:rbac:groups=environments.arc.ucl,resources=vaultk8sconfigs/finalizers,verbs=update
 // +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create
 // +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create
+// +kubebuilder:rbac:groups="",resources=serviceaccounts/token,verbs=create
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=get;list;watch;create;bind;escalate
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=get;list;watch;create
